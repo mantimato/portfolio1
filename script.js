ac40964ebd4eec9aad023a5f8f1d680786ff0c04
Mantas <mantimato@gmail.com>
2017-10-02 23:50:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+		
+	});
+	
+	$('.nav-icona').click(function(){
+		$(".menujs").toggle(100);
+		
+	});
+});
